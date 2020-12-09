301d1fa3f0aa44f770bedda36d7aa94156cb19a4
Jerson Q <solorsanolopez@gmail.com>
2020-12-09 01:01:22 -0800

    [Add] get user data endpoint

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -101,4 +101,12 @@ router.post('/tokenIsValid', async (req, res) => {
   }
 });
 
+router.get('/', auth, async (req, res) => {
+  const user = await User.findById(req.user);
+  res.json({
+    displayName: user.displayName,
+    id: user._id,
+  });
+});
+
 module.exports = router;
