ba0bace306104668946450c627b2140e26d7469a
Jerson Q <solorsanolopez@gmail.com>
2021-01-07 19:53:25 -0800

    [Add] badge model and badge router

---
+++ models/badgeModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const badgeSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  jobTitle: { type: String, required: true },
+  categorie: { type: String, required: true },
+  badgeImage: { type: String },
+});
+
+module.exports = Badge = mongoose.model('badge', badgeSchema);
---
+++ routes/badgeRouter.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const auth = require('../middleware/auth');
+const Badge = require('../models/badgeModel');
+const multer = require('multer');
+
+router.post();
+
+module.exports = router;
